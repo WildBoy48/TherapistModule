33a6cbf0788e72ea59fe9d51e8cae5cc0e948f3d
Nuno Covão <nunobipbip@gmail.com>
2026-03-11 19:33:53 +0000

    Firebase setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,25 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'therapist-module',
+        appId: '1:931936192365:web:cc0d6255d7673b33ff7c64',
+        storageBucket: 'therapist-module.firebasestorage.app',
+        apiKey: 'AIzaSyApqMhm8L_nZUWSNNsFc8cGIbFufk-wdvI',
+        authDomain: 'therapist-module.firebaseapp.com',
+        messagingSenderId: '931936192365',
+        projectNumber: '931936192365',
+        version: '2',
+      }),
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
